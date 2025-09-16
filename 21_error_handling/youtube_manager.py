0b62f86d56649a8d924364b8327c911e4cbbffa1
ShivomSharma2000 <shivomsharma2362000@gmail.com>
2025-09-16 23:23:56 +0530

    latest code

---
+++ 21_error_handling/youtube_manager.py
@@ -0,0 +1,36 @@
+def list_all_videos(videos):
+    pass
+
+def add_video(video):
+    pass
+
+def update_video(video):
+    pass
+
+def delete_video(video):
+    pass
+
+videos = []
+
+while True:
+    print("\n Youtube Manager | Choose an option")
+    print("1. List all Youtube videos")
+    print("2. Add a Youtube video")
+    print("3. Update a Youtube video details")
+    print("4. Delete a Youtube video")
+    print("5. Exist the app")
+    choice = input("Enter your choice")
+
+    match choice:
+        case '1':
+            list_all_videos(videos)
+        case '2':
+            add_video(videos)
+        case '3':
+            update_video(videos)
+        case '4':
+            delete_video(videos)
+        case '5':
+            break
+        case _:
+            print("Invalid choice")
