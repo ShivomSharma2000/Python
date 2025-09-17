ec66b61157a6762d814afc639b61950a8110c455
ShivomSharma2000 <shivomsharma2362000@gmail.com>
2025-09-18 00:02:19 +0530

    now starting 21st video of python

--- 21_error_handling/youtube_manager.py
+++ 21_error_handling/youtube_manager.py
@@ -33,10 +33,27 @@ def add_video(videos):
     save_data_helper(videos)
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update: "))
+
+    if 1 <= index <= len(videos):
+        name = input("Enter new video name: ")
+        time = input("Enter new video time: ")
+        videos[index - 1] = {'name':name, 'time':time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to delete: "))
+
+    if 1 <= index <= len(videos):
+        del videos[index - 1] 
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
+
 
 def main():
     videos = load_data()
@@ -47,7 +64,7 @@ def main():
         print("3. Update a Youtube video details")
         print("4. Delete a Youtube video")
         print("5. Exist the app")
-        choice = input("Enter your choice")
+        choice = input("Enter your choice: ")
 
         match choice:
             case '1':
